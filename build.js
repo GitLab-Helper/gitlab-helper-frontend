68f43ccca433ac2b24176fa0a1c56a5fabe2be49
Mateusz Gorczycki <mateusz.gorczycki@whiteaster.com>
2020-07-17 08:34:55 +0200

    Create build.js and PROD version of app

---
+++ build.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+
+const PROD = process.env.PROD == "true" || false;
+
+const filePath = PROD ? "./config.json" : "./config.json";
+
+let rawdata = fs.readFileSync(filePath);
+let config = JSON.parse(rawdata);
+
+Object.keys(config).forEach((key) => {
+  if (process.env[key.toUpperCase()]) {
+    config[key] = process.env[key.toUpperCase()];
+  }
+});
+
+fs.writeFileSync(filePath, JSON.stringify(config));
